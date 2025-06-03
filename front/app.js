27c395e2894efa07c2f37c02077179ef0d022f5e
captain798 <2838651487@qq.com>
2025-06-03 13:54:50 +0800

    弹窗的完善

--- front/app.js
+++ front/app.js
@@ -7,8 +7,7 @@ App({
   // 全局共享数据
   globalData: {
       userInfo: null,    // 用户信息对象
-      userName : null,   // 用户姓名
-      userCard : null,   // 学工号
+      accessToken: null, // 登录凭证
   },
 
   onLaunch() {
@@ -17,17 +16,6 @@ App({
       const logs = wx.getStorageSync('logs') || [];
       logs.unshift(Date.now());
       wx.setStorageSync('logs', logs);
-      
-      
-      // 检查登录token
-      const token = wx.getStorageSync('access_token');
-      if (!token) {
-          wx.redirectTo({
-              url: '/pages/login/login',
-              success: () => console.log('跳转登录页成功'),
-              fail: (err) => console.error('跳转失败:', err)
-          });
-      }
 
       // 加载用户缓存数据
       wx.getStorage({
