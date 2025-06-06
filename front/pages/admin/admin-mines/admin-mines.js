52beca5552e61e5943862fd93dfdd3cc4d1102a6
captain798 <2838651487@qq.com>
2025-06-06 21:49:33 +0800

    feat(admin): 添加数据导出页面导航功能

--- front/pages/admin/admin-mines/admin-mines.js
+++ front/pages/admin/admin-mines/admin-mines.js
@@ -4,4 +4,13 @@ Page({
     adminName : "网小安"
   },
 
+  /**
+   * 导航到数据导出页面
+   */
+  navigateToExport: function() {
+    wx.navigateTo({
+      url: '/pages/admin/export/export'
+    });
+  },
+
 })
