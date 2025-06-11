68626dd034f7b9b2c8934ec1798f73bdcd79feed
captain798 <2838651487@qq.com>
2025-06-12 00:59:47 +0800

    燃尽了，晚安

--- front/pages/admin/admin-mines/admin-mines.js
+++ front/pages/admin/admin-mines/admin-mines.js
@@ -1,14 +1,47 @@
 Page({
   onShow: function () {
-    if (typeof this.getTabBar === 'function' &&  this.getTabBar()) {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
         selected: 1
-      })
-    };
+      });
+    }
+    this.loadStatistics();
   },
 
   data: {
-    adminName : "管理员"
+    adminName: "管理员",
+    dateRange: [new Date().toISOString().split('T')[0], new Date().toISOString().split('T')[0]],
+    statistics: [],
+    analysis: {},
+    loading: false
+  },
+
+  onDateChange: function(e) {
+    this.setData({
+      dateRange: e.detail.value,
+      loading: true
+    });
+    this.loadStatistics();
+  },
+
+  loadStatistics: function() {
+    const [startDate, endDate] = this.data.dateRange;
+    wx.request({
+      url: 'https://your-api-domain.com/admin/statistics',
+      method: 'GET',
+      data: { start_date: startDate, end_date: endDate },
+      success: (res) => {
+        this.setData({
+          statistics: res.data.statistics,
+          analysis: res.data.analysis,
+          loading: false
+        });
+      },
+      fail: () => {
+        wx.showToast({ title: '获取数据失败', icon: 'none' });
+        this.setData({ loading: false });
+      }
+    });
   },
 
   navigateToExport: function() {
@@ -26,5 +59,4 @@ Page({
       url: '/pages/admin/orders/orders'
     });
   }
-
-})
+});
